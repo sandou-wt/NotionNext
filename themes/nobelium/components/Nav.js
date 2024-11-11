8e3c3e28c15f2a9898d945b140e0f755ec1ac0f3
sandou-wt <83268537+sandou-wt@users.noreply.github.com>
2024-11-11 17:05:05 +0800

    Update Nav.js

--- themes/nobelium/components/Nav.js
+++ themes/nobelium/components/Nav.js
@@ -67,7 +67,7 @@ const Nav = props => {
               )}
             </div>
           </Link>
-          <p className='ml-2 font-medium text-gray-800 dark:text-gray-300 header-name whitespace-nowrap'>
+          <p className='ml-2 text-xl font-bold text-gray-800 dark:text-gray-300 header-name whitespace-nowrap'>
             {siteConfig('TITLE')}
             {/* ,{' '}<span className="font-normal">{siteConfig('DESCRIPTION')}</span> */}
           </p>
