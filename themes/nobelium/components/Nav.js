a9be5e4dd85500e1f1777051fd0644cc95df0a72
sandou-wt <wangt0415_douzi@163.com>
2024-11-13 13:41:22 +0800

    fix

--- themes/nobelium/components/Nav.js
+++ themes/nobelium/components/Nav.js
@@ -60,7 +60,7 @@ const Nav = props => {
                 width={28}
                 height={28}
                 alt={siteConfig('TITLE')}
-                className='mb-[2px]'
+                className='mb-[3px]'
                 />
             </div>
           </Link>
