371924dd0d91449e94a73826ddbe06fb6455f709
sandou-wt <wangt0415_douzi@163.com>
2025-02-11 17:08:59 +0800

    修改简介

--- blog.config.js
+++ blog.config.js
@@ -25,7 +25,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Sandou', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '挥着紫色蜡笔前行', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://sandou.site', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, sandou, 忒修斯之船', // 网站关键词 英文逗号隔开
 
